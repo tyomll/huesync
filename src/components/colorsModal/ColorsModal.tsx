f9f237599d409998762f768cd5ea22efedb1f1f2
tyommmm <artyomhovsepyan135@gmail.com>
2023-06-09 00:07:28 +0400

    Fixed colors modal for mobile devices.

--- src/components/colorsModal/ColorsModal.tsx
+++ src/components/colorsModal/ColorsModal.tsx
@@ -9,9 +9,9 @@ const ColorsModal = () => {
 	const shadow = { boxShadow: "0px 5px 0px #191A23" };
 
 	return (
-		<div className="flex items-center justify-center fixed z-30 left-0 top-0 w-[100vw] h-[100vh] bg-[#ffffff66]">
+		<div className="flex items-start justify-center fixed z-30 left-0 top-0 w-[100vw] h-[100vh] bg-[#ffffff66]">
 			<div
-				className="relative w-[70%] h-[60%] bg-secondaryButton rounded-[45px] border-[1px] border-primaryButton py-[30px] px-[30px]"
+				className="relative w-[70%] h-[60%] mt-[80px] bg-secondaryButton rounded-[45px] border-[1px] border-primaryButton py-[30px] px-[30px]"
 				style={shadow}
 			>
 				<div className="text-center grid grid-cols-2 w-full h-full gap-[5px] ">
