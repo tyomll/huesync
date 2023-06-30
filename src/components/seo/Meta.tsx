d448c7f72f170a8a0e330e2fa47b8707bd1d820e
tyommmm <artyomhovsepyan135@gmail.com>
2023-06-30 23:19:05 +0400

    Added favico.

--- src/components/seo/Meta.tsx
+++ src/components/seo/Meta.tsx
@@ -1,8 +1,9 @@
 import React, { FC, PropsWithChildren } from "react";
 import { IMeta } from "./meta.interface";
 import Head from "next/head";
+import favico from "../../assets/logo.ico";
 
-const getTitle = (title: string) => `${title} | Mate`;
+const getTitle = (title: string) => `${title} | Huesync`;
 
 const Meta: FC<PropsWithChildren<IMeta>> = ({
 	title,
@@ -13,6 +14,8 @@ const Meta: FC<PropsWithChildren<IMeta>> = ({
 		<>
 			<Head>
 				<title>{getTitle(title)}</title>
+				<link rel="shortcut icon" href={favico.src} />
+
 				{description ? (
 					<>
 						<meta name="description" content={description} />
